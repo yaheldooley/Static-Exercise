f7968bc3f2cf9b78be45fe77717731e91ecfcf9b
yahel dooley <ydooley@gmail.com>
2022-10-26 12:01:33 -0700

    Add static TempConverter class with methods converting C to F and F to C

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			Console.WriteLine($"{32} F = {TempConverter.FahrenheitToCelsius(32)} C");
+			Console.WriteLine($"{0} C = {TempConverter.CelsiusToFahrenheit(0)} F");
+		}
     }
+
+    public static class TempConverter
+    {
+		public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return (c * 9) / 5 + 32;
+        }
+	}
 }
